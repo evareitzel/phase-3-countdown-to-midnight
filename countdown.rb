61f0ccdfee71503b28e0b9aaea966f1d62886af1
Eva Reitzel <evareitzel@gmail.com>
2022-11-12 15:42:08 -0500

    commit tree 3b6ba470f0a95b340b9b47b6976ace42747f6979

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown count
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+  end
+  "Happy new year!".upcase
+end
+
+def countdown_with_sleep num
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1  
+    sleep 1
+  end
+  "Happy new year!".upcase
 end
